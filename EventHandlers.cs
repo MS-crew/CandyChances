64989f057ab1638ddc49d1fc775554caf3b6d03a
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-05-01 22:28:23 +0300

    real fix

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -2,6 +2,8 @@
 using Exiled.CustomRoles.API;
 using Exiled.Events.EventArgs.Scp330;
 using Exiled.CustomRoles.API.Features;
+using InventorySystem.Items.Usables.Scp330;
+using Interactables.Interobjects;
 
 namespace CandyChances
 {
@@ -12,38 +14,40 @@ namespace CandyChances
             if (!ev.IsAllowed)
                 return;
 
-            int UsageLimit = 2;
+            int UsageLimit;
             CustomRole customRole = ev.Player.GetCustomRoles()?.FirstOrDefault();
             
             if (customRole != null && Plugin.Instance.Config.ModifiedUseLimitsforCustomRoles.TryGetValue(customRole.Name, out UsageLimit))
-            {
                 ev.ShouldSever = ev.UsageCount >= UsageLimit;
-            }
             else if (Plugin.Instance.Config.ModifiedUseLimits.TryGetValue(ev.Player.Role.Type, out UsageLimit))
-            {
                 ev.ShouldSever = ev.UsageCount >= UsageLimit;
-            }
+            else
+                UsageLimit = Scp330Interobject.MaxAmountPerLife;
 
+
+            ev.ShouldPlaySound = Plugin.Instance.Config.ShouldPlayTakeSound;
 
             if (ev.ShouldSever)
             {
-                ev.Player.ShowHint(Plugin.Instance.Translation.HandsSeveredHints.RandomItem(), 
-                                   Plugin.Instance.Config.HandsSeveredHintTime);
+                if (!Plugin.Instance.Config.HandsSeveredHint)
+                    return;
+
+                ev.Player.ShowHint(Plugin.Instance.Translation.HandsSeveredHints.RandomItem(), Plugin.Instance.Config.HandsSeveredHintTime);
+                return;
             }
-            else if (Plugin.Instance.Translation.GetCandyHints.TryGetValue(ev.Candy, out string[] candyHints))
+
+            if (!Plugin.Instance.Translation.GetCandyHints.TryGetValue(ev.Candy, out string[] candyHints))
+                return;
+
+            string hint = candyHints.RandomItem();
+
+            if (Plugin.Instance.Config.ShowRemainingUse)
             {
-                string hint = candyHints.RandomItem();
-
-                if (Plugin.Instance.Config.ShowRemainingUse)
-                {
-                    int remaining = UsageLimit - ev.UsageCount - 1;
-                    hint += Plugin.Instance.Translation.RemainingUse.Replace("{0}", remaining.ToString());
-                } 
-
-                ev.Player.ShowHint(hint, Plugin.Instance.Config.CandyHintTime);
+                int remaining = UsageLimit - ev.UsageCount - 1;
+                hint += Plugin.Instance.Translation.RemainingUse.Replace("{0}", remaining.ToString());
             }
 
-            ev.ShouldPlaySound = Plugin.Instance.Config.ShouldPlayTakeSound;
+            ev.Player.ShowHint(hint, Plugin.Instance.Config.CandyHintTime);
         }
     }
 }
